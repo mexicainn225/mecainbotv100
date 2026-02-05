a8a2a77d347a6dbfc20e47768e01b2c6bbb25b52
mexicainn225 <mexicainchoco0319@gmail.com>
2026-02-05 01:06:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def get_universal_signal():
     
     next_sig_total = base_minute
     while next_sig_total <= total_minutes_now:
-        next_sig_total += 21 
+        next_sig_total += 17 
         
     target_hour = (next_sig_total // 60) % 24
     target_minute = next_sig_total % 60
