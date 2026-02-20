2637d8e541a5f6b6d8fb8d0996a4b43fb128905e
mexicainn225 <mexicainchoco0319@gmail.com>
2026-02-20 10:29:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def get_next_signal():
     sig_total = base_min
     
     while sig_total <= total_now:
-        sig_total += 12
+        sig_total += 31
     
     target_time = now.replace(hour=(sig_total // 60) % 24, minute=sig_total % 60, second=0, microsecond=0)
     
