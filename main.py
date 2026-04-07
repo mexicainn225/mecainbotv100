f4f84b21f030bac3c7b7a59d55aca3c56136e9a1
mexicainn225 <mexicainchoco0319@gmail.com>
2026-04-07 12:25:31 +0000

    Fix increment operation in while loop

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def get_next_signal():
     # Intervalle de 21 minutes
     sig_total = base_min
     while sig_total <= total_now:
-        sig_total += 10
+        sig_total +=10
         
     target_hour = (sig_total // 60) % 24
     target_minute = sig_total % 60
